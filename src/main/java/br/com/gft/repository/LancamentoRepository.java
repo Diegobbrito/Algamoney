15c255d0651bd17763122d8d47290201f45a4c2b
dobb <diego.brito@gft.com>
2020-11-19 14:45:10 -0300

    Paginação Lancamento

--- src/main/java/br/com/gft/repository/LancamentoRepository.java
+++ src/main/java/br/com/gft/repository/LancamentoRepository.java
@@ -3,8 +3,9 @@ package br.com.gft.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import br.com.gft.model.Lancamento;
+import br.com.gft.repository.lancamento.LancamentoRepositoryQuery;
 
-public interface LancamentoRepository extends JpaRepository<Lancamento, Long> {
+public interface LancamentoRepository extends JpaRepository<Lancamento, Long>, LancamentoRepositoryQuery {
 
 }
  
