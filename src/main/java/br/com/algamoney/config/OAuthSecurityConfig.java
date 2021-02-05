4eff5aa977f858198078ba45ece796a9e6533010
DiegoBrito <dibbrito@gmail.com>
2021-02-04 22:26:16 -0300

    Update Oauth

--- src/main/java/br/com/algamoney/config/OAuthSecurityConfig.java
+++ src/main/java/br/com/algamoney/config/OAuthSecurityConfig.java
@@ -1,6 +1,5 @@
 package br.com.algamoney.config;
 
-import br.com.algamoney.config.property.AlgamoneyApiProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
